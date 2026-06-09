4ff4e12c96767d57dfb617c6ce189f02e8089fb8
Faheem Khanzada <faheem@sky.com>
2026-06-09 15:57:57 +0100

    new changes

--- zymaya/settings.py
+++ zymaya/settings.py
@@ -25,7 +25,9 @@ TEMPLATES=[{
 WSGI_APPLICATION='zymaya.wsgi.application'
 DATABASES={'default':{'ENGINE':'django.db.backends.sqlite3','NAME':BASE_DIR/'db.sqlite3'}}
 LANGUAGE_CODE='en-gb'; TIME_ZONE='Europe/London'; USE_I18N=True; USE_TZ=True
-STATIC_URL='static/'; STATIC_ROOT=BASE_DIR/'staticfiles'
-MEDIA_URL='media/'; MEDIA_ROOT=BASE_DIR/'media'
+STATIC_URL='static/'; 
+STATIC_ROOT=BASE_DIR/'staticfiles'
+MEDIA_URL='media/'; 
+MEDIA_ROOT=BASE_DIR/'media'
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR / "staticfiles"
