29786b54e5838371612f9a4f120e0cd26907d5a4
Faheem Khanzada <faheem@sky.com>
2026-06-09 16:01:24 +0100

    new changes

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from jupyterlab_server import slugify
+from django.utils.text import slugify
 
 class Category(models.Model):
     name=models.CharField(max_length=120)
