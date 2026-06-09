47596f9c2b657973f6aaae82866a5cc42a66006d
Faheem Khanzada <faheem@sky.com>
2026-06-09 16:06:52 +0100

    new changes

--- zymaya/settings.py
+++ zymaya/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'dev-only-zymaya-change-me'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["zymaya.onrender.com"]
 INSTALLED_APPS = [
     'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
     'django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','store',
